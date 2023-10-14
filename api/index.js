0399b42badd102ac7a848e04642d5d56a66f5335
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-10-14 19:51:44 +0530

    cors3

--- api/index.js
+++ api/index.js
@@ -24,7 +24,12 @@ app.use('/uploads',express.static(__dirname+'/uploads'))
 
 app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173', // Allow requests from localhost:5173
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  };
+  
+  app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
     console.log('Cookies:', req.cookies);
