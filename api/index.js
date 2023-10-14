adc6abe57a20352351dd9de83436e47b204ec6ba
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-10-14 19:18:58 +0530

    checking it

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 mongoose.connect(process.env.MONGO_URL);
 
 app.get('/', (req, res) => {
-        res.json('test');
+        res.json('test2');
 });
   
 
@@ -277,18 +277,18 @@ app.get('/bookings',async (req,res)=>{
 })
 
 //-----------------------------------
-const __dirname1 = path.resolve(__dirname);
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, 'client', 'dist')));
+// const __dirname1 = path.resolve(__dirname);
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname1, 'client', 'dist')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'dist', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.json('test');
-    });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, 'client', 'dist', 'index.html'));
+//     });
+// } else {
+//     app.get('/', (req, res) => {
+//         res.json('test');
+//     });
+// }
 
 //-----------------------------------
 
