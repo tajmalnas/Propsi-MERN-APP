605ed806e27311ba8bec136e6ceeb042e10e1afd
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-11-11 22:30:06 +0530

    mount

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -10,7 +10,6 @@ export function UserContextProvider({ children }) {
 
     useEffect(() => {
         if (!user) {
-            let isMounted = true; // Flag to check if the component is still mounted
             const token = localStorage.getItem('token');
 
             axios.get('/profile', {
@@ -19,25 +18,19 @@ export function UserContextProvider({ children }) {
                 }
             })
             .then(({ data }) => {
-                if (isMounted) {
-                    // Check if the component is still mounted before updating state
-                    if (!data) {
-                        setUser(null);
-                    } else {
-                        setUser(data);
-                    }
-                    setReady(true);
+                console.log(data);
+                if (!data) {
+                    setUser(null);
+                } 
+                else {
+                    setUser(data);
                 }
+                setReady(true);
             })
             .catch(error => {
                 // Handle the error, e.g., show an error message or redirect to an error page
                 console.error('Error fetching user data:', error);
             });
-
-            // Cleanup function to set isMounted to false when the component is unmounted
-            return () => {
-                isMounted = false;
-            };
         }
     }, [user]); // Include user in the dependency array if you want the effect to run on user changes
 
