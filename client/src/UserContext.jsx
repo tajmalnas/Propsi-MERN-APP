7f0174d739e7c7f04f7705958e892d9dfb6343b8
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-11-11 13:08:19 +0530

    user context

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -4,49 +4,46 @@ import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({children}){
-    
+export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
     const [ready, setReady] = useState(false);
-    
+
     useEffect(() => {
         if (!user) {
             let isMounted = true; // Flag to check if the component is still mounted
             const token = localStorage.getItem('token');
-            axios.get('/profile',{
+
+            axios.get('/profile', {
                 headers: {
-                Authorization: `${token}`,
-            }
-            }
-            )
-                .then(({ data }) => {
-                    if (isMounted) {
-                        // Check if the component is still mounted before updating state
-                        if (Object.keys(data).length === 0) {
-                            setUser(null);
-                        } else {
-                            setUser(data);
-                        }
-                        setReady(true);
+                    Authorization: `${token}`,
+                }
+            })
+            .then(({ data }) => {
+                if (isMounted) {
+                    // Check if the component is still mounted before updating state
+                    if (!data) {
+                        setUser(null);
+                    } else {
+                        setUser(data);
                     }
-                })
-                .catch(error => {
-                    // Handle the error, e.g., show an error message or redirect to an error page
-                    console.error('Error fetching user data:', error);
-                });
-            
+                    setReady(true);
+                }
+            })
+            .catch(error => {
+                // Handle the error, e.g., show an error message or redirect to an error page
+                console.error('Error fetching user data:', error);
+            });
+
             // Cleanup function to set isMounted to false when the component is unmounted
             return () => {
                 isMounted = false;
             };
         }
-    }, []); // Empty dependency array to run the effect only once, similar to componentDidMount
-
-    
+    }, [user]); // Include user in the dependency array if you want the effect to run on user changes
 
     return (
-        <UserContext.Provider value={{user,setUser,ready}}>
+        <UserContext.Provider value={{ user, setUser, ready }}>
             {children}
         </UserContext.Provider>
-    )
+    );
 }
